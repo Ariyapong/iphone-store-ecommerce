94c3d97f6324dc0e5039b1763b5f0904bbcc7144
Ariyapong W <41046551+Ariyapong@users.noreply.github.com>
2023-02-25 10:21:32 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- <Tutorial/> -->
+    <div class="text-blue-600 flex">Hello sawasdee krub {{ sampleVal }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "IndexPage",
+
+  asyncData({ $axios }){
+    console.log('check axios : ', $axios)
+
+    return {
+      sampleVal: 'eiei'
+    }
+  }
+});
+</script>
