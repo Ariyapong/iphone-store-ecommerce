4ce0ecf38062475a8b5b3b287b0e36ae8a0aa2f4
Ariyapong W <41046551+Ariyapong@users.noreply.github.com>
2023-02-27 19:54:16 +0700

    add lib

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ const appConfig: NuxtConfig = {
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
-
+  ssr: true,
   // Global CSS: https://go.nuxtjs.dev/config-css
   // css: ["~/assets/styles/main.scss"],
   css: ["./assets/styles/main.scss"],
